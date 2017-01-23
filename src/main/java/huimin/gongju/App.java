e27981355bc2cbe20c3db51cefb9211115c98fa8
LindaYack <litterature@sina.cn>
2017-01-23 09:38:18 +0800

    Add files via upload

---
+++ src/main/java/huimin/gongju/App.java
@@ -0,0 +1,13 @@
+package huimin.gongju;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
